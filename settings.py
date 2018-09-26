25dc84a5c52bb28723d4139d5fd2da078ea1c05f
Vladimir Korotkov <kva@delrus.ru>
2018-09-26 15:33:12 +0500

    fixed some pylint issues

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 # Путь к файлу Ozi Explorer Waypoint
-OZI_WAYPOINTS_FILE = 'd:\OziExplorer\data\CADASTER.txt'
+OZI_WAYPOINTS_FILE = r'd:\OziExplorer\data\CADASTER.txt'
 
 # Путь к папке с отчетами по изысканиям
-REPORTS_PATH = 'D:\Home System\ИЗЫСКАНИЯ'
+REPORTS_PATH = r'D:\Home System\ИЗЫСКАНИЯ'
 
 # Путь к файлу с TeX файлом - шаблоном отчета по изысканиям
-TEX_TEMPLATE_PATH = 'D:\GIT-REPOS\latex-templates\water'
-TEX_TEMPLATE_FILE = 'water.tex'
+TEX_TEMPLATE_PATH = r'D:\GIT-REPOS\latex-templates\water'
+TEX_TEMPLATE_FILE = r'water.tex'
 
 # URL API Росеестра
 # URL_ROSEESTR_API = 'http://pkk5.rosreestr.ru/api/features/1/'
